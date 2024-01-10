a91b19ab85563de55c32313c5e43121bab7b3147
hironmoy-ray <hpray35@gmail.com>
2024-01-10 21:07:16 +0600

    Added useState & React Component(<Function/>

--- js/react.js
+++ js/react.js
@@ -20,38 +20,46 @@
 ////////////////////////////////////////////////////////////
 
 // Creating Element with JSX (Transpiler -> Babel)
-const productCart = (
-    <div class="shadow-md rounded-lg bg-slate-800 border-slate-700">
-    <a href="#">
-        <img
-            class="rounded-t-lg"
-            src="https://dummyimage.com/680X400/087ea4/ffffff.png&text=Product"
-            alt="product image"
-        />
-    </a>
-    <div class="px-5 py-5">
-        <div class="flex items-center justify-between mt-5">
-            <a href="#">
-                <h3 class="font-semibold text-xl tracking-tight">
-                    Product Prize
-                </h3>
-            </a>
-            <span class="text-xl font-medium text-teal-500"
-                >Total: ৳ 5000</span
-            >
-        </div>
-        <div class="flex items-center justify-between mt-5">
-            <span class="text-2xl font-bold text-slate-300"
-                >৳ 5000</span
-            >
-            <a
-                href="#"
-                class="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#087ea4] hover:bg-[#087ea4]/[.8] focus:ring-[#087ea4]/[.5]"
-                >Add to cart</a
-            >
+const productPrice = 500;
+
+function Product(){
+    const [quantity,setQuantity] = React.useState(0);
+
+    function addToCart(){
+        setQuantity(quantity+1);
+    }
+
+    return (
+        <div className="shadow-md rounded-lg bg-slate-800 border-slate-700">
+        <a href="#">
+            <img
+                className="rounded-t-lg"
+                src="https://dummyimage.com/680X400/087ea4/ffffff.png&text=Product"
+                alt="product image"
+            />
+        </a>
+        <div className="px-5 py-5">
+            <div className="flex items-center justify-between mt-5">
+                <a href="#">
+                    <h3 className="font-semibold text-xl tracking-tight">
+                        Product Prize
+                    </h3>
+                </a>
+                <span className="text-xl font-medium text-teal-500"
+                    >Total: ৳ {quantity * productPrice}</span>
+            </div>
+            <div className="flex items-center justify-between mt-5">
+                <span className="text-2xl font-bold text-slate-300">৳ {productPrice}</span>
+                <a
+                    onClick={addToCart}
+                    href="#"
+                    className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#087ea4] hover:bg-[#087ea4]/[.8] focus:ring-[#087ea4]/[.5]">
+                        Add to cart </a>
+            </div>
         </div>
     </div>
-</div>
-);
+    );
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(productCart);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Product/>);
